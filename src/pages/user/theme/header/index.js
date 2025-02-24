1248bf46b565dd87ee435f14e74aa5725fd63a5b
DinhVanNhan <vannhan10102004@gmail.com>
2025-02-25 00:12:44 +0700

    feat(b7): coding header - menus.

--- src/pages/user/theme/header/index.js
+++ src/pages/user/theme/header/index.js
@@ -1,4 +1,4 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import "./style.scss"
 import { Link } from "react-router-dom";
 import { formater } from "../../../../utils/formater";
@@ -7,9 +7,54 @@ import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaSquareTwitter } from "react-icons/fa6";
 import { FaUser } from "react-icons/fa";
+import { IoCartOutline } from "react-icons/io5";
 
 import { IoMailUnreadOutline } from "react-icons/io5";
+import { ROUTERS } from "../../../../utils/router";
 const Header = () => {
+
+    const [menus, setMenus] = useState([
+        {
+            name: "Trang chủ",
+            path: ROUTERS.USER.HOME,
+        },
+        {
+            name: "Cửa hàng",
+            path: ROUTERS.USER.PRODUCTS,
+        },
+        {
+            name: "Sản phẩm",
+            path: ROUTERS.USER.PRODUCTS,
+            isShowSubMenu: false,
+            child: [
+                {
+                    name: "Thịt",
+                    path: ""
+                },
+                {
+                    name: "Rau củ",
+                    path: ""
+                },
+                {
+                    name: "Đồ uống",
+                    path: ""
+                },
+                {
+                    name: "Thức ăn nhanh",
+                    path: ""
+                },
+            ]
+        },
+        {
+            name: "Bài viết",
+            path: ROUTERS.USER.POST,
+        },
+        {
+            name: "Liên hệ",
+            path: ROUTERS.USER.CONTACT,
+        },
+    ])
+
     return (
         <>
             <div className="header_top">
@@ -63,14 +108,44 @@ const Header = () => {
             </div>
             <div className="container">
                 <div className="row">
-                    <div className="col-xl-3">
-                        LOGO
+                    <div className="col-xl-3 col-lg-3 col-sm-3">
+                        <div className="header__logo">
+                            <h2>Dvnsmart</h2>
+                        </div>
                     </div>
-                    <div className="col-xl-6">
-                        MENUS
+                    <div className="col-xl-6 col-lg-3 col-sm-3">
+                        <nav className="header__menu">
+                            <ul>
+                                {
+                                    menus?.map((menu, menuKey) => {
+                                        return <>
+                                            <li key={menuKey} className={menuKey === 0 ? "active" : ""}>
+                                                <Link to={menu?.path}>{menu?.name}</Link>
+                                                <ul>
+                                                </ul>
+                                            </li>
+                                        </>
+                                    })
+                                }
+                            </ul>
+                        </nav>
                     </div>
-                    <div className="col-xl-3">
-                        PHONE
+                    <div className="col-xl-3 col-lg-3 col-sm-3">
+                        <div className="header__cart">
+                            <div className="header__cart__price">
+                                <span>{formater(1500000)}</span>
+                            </div>
+                            <ul>
+                                <li>
+                                    <Link to={"#"}>
+                                        <IoCartOutline />
+                                    </Link>
+                                    <span>
+                                        5
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
